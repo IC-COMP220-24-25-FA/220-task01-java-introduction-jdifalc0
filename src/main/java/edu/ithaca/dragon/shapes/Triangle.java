af916ce5392d5b3df8021981c54419f6e5c13e9a
Joseph DiFalco <jdifalco@ithaca.edu>
2024-09-02 18:48:14 -0400

    finished Triangle class(sorry for no previous commits)

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,35 @@
+package edu.ithaca.dragon.shapes;
+
+import java.lang.classfile.instruction.ThrowInstruction;
+
+public class Triangle {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    /**
+     * @throws IllegalArgumentException if radius is not a positive number
+     */
+    public double calcArea(){
+        double s = (sideA + sideB + sideC)/2;
+        return Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+
+    }
+
+    /**
+     * @post doubles the size of this circle
+     */
+    public void doubleSize(){
+        sideA= sideA *1.5;
+        sideB=sideB *1.5;
+        sideC=sideC * 1.5;
+    }
+
+    /**
+     * @return the longest straight line that can be drawn within this circle
+     */
+    public double longestLineWithin(){
+        double area = calcArea();
+        return 2*(area/sideB);
+    }
+}
