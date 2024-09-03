1d87f15c7572b82c29d7a4e3709da836b44ba410
Joseph DiFalco <jdifalco@ithaca.edu>
2024-09-03 00:42:05 -0400

    implemented main

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,4 +1,5 @@
 package edu.ithaca.dragon.shapes;
+import java.util.Scanner; 
 
 public class ShapeMain {
     
@@ -6,5 +7,25 @@ public class ShapeMain {
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+        Rectangle[] rectangles = new Rectangle[5];
+        rectangles[0] = new Rectangle(Math.random(),Math.random());
+        rectangles[1] = new Rectangle(Math.random(),Math.random());
+        rectangles[2] = new Rectangle(Math.random(),Math.random());
+        rectangles[3] = new Rectangle(Math.random(),Math.random());
+        rectangles[4] = new Rectangle(Math.random(),Math.random());
+        // Must type 1,2,3,4 or 5
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Choose one: Rectangle 1 Area:"+rectangles[0].calcArea()+"Rectangle 1 Longest Line:"
+            +rectangles[0].longestLineWithin() +"Rectangle 2 Area:"+rectangles[1].calcArea()+"Rectangle 2 Longest Line:"
+            +rectangles[1].longestLineWithin() +"Rectangle 3 Area:"+rectangles[2].calcArea()+"Rectangle 3 Longest Line:"
+            +rectangles[2].longestLineWithin() +"Rectangle 4 Area:"+rectangles[3].calcArea()+"Rectangle 4 Longest Line:"
+            +rectangles[3].longestLineWithin() +"Rectangle 5 Area:"+rectangles[4].calcArea()+"Rectangle 5 Longest Line:"
+            +rectangles[4].longestLineWithin() ) ;
+            Scanner myObj = new Scanner(System.in);
+            int choiceint= myObj.nextInt();
+            rectangles[choiceint].doubleSize();
+        }
+
+
     }
 }
